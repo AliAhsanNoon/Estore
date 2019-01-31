58cc88ce59a1f33e88c3197981a0f041c367766a
Ali Ahsan Noon <alinoonciit@gmail.com>
2019-01-31 12:35:12 +0500

    Add CategoryService

---
+++ com.services/CategoryService.cs
@@ -0,0 +1,37 @@
+﻿using com.database;
+using com.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.services
+{
+    public class CategoryService
+    {
+        CContext context = new CContext();
+
+        public List<Category> GetProducts()
+        {
+            return context.Categories.ToList();
+        }
+
+        public Category Edit(int ID)
+        {
+            using (context)
+            {
+                return context.Categories.ToList().SingleOrDefault(x => x.ID == ID);
+            }
+        }
+
+        public void SaveCategory(Category category)
+        {
+            using (context)
+            {
+                context.Categories.Add(category);
+                context.SaveChanges();
+            }
+        }
+    }
+}
