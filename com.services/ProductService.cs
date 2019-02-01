7beaddd97b5d8fc92507f70543dd1fb9b6200ad6
Ali Ahsan Noon <alinoonciit@gmail.com>
2019-02-01 13:50:44 +0500

    Updated Services , Controllers and Views

---
+++ com.services/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using com.database;
+using com.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace com.services
+{
+    public class ProductService
+    {
+        CContext context = new CContext();
+
+        public List<Product> GetProducts()
+        {
+            return context.Products.ToList();
+        }
+    }
+}
