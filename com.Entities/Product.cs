88b3d74e29c2c6d46f04c46fa2f70ddd83663517
Ali Ahsan Noon <alinoonciit@gmail.com>
2019-02-19 20:02:14 +0500

    Data Validation

--- com.Entities/Product.cs
+++ com.Entities/Product.cs
@@ -1,7 +1,10 @@
-﻿namespace com.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace com.Entities
 {
     public class Product : BaseEntity
     {
+        [Range(1,10000)]
         public decimal Price { get; set; }
 
         public Category Category { get; set; }
